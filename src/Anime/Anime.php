0cdb790f1706b8cf6512dbe5b3d9d93306e4f086
andikabahari <38852468+andikabahari@users.noreply.github.com>
2019-05-20 11:25:28 +0700

    Create Anime.php

---
+++ src/Anime/Anime.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * Anime Class
+ *
+ * @author Andika Bahari
+ * @license MIT License
+ */
+class Anime
+{
+
+  /**
+   * @var string
+   */
+  private $baseURL = 'https://myanimelist.net/anime/';
+
+  /**
+   * @var string
+   */
+  private $searchURL = 'https://myanimelist.net/anime.php?q=';
+
+  /**
+   * @var string
+   */
+  private $ajaxURL = 'https://myanimelist.net/includes/ajax.inc.php?t=64&id=';
+
+  /**
+   * @param string $keyword
+   * @param bool $jsonMode
+   * @param int $limit
+   * @return mixed
+   */
+  public function search($keyword, $jsonMode, $limit)
+  {
+    $ids = $this->getAnimeIds($this->searchURL . $keyword);
+    $details = array();
+
+    // Max execution time is 3600 seconds
+    ini_set('max_execution_time', 3600);
+
+    $index = 0;
+    for ($index; $index < $limit; $index++)
+    {
+      $details[] = $this->getAnimeDetails($this->ajaxURL . $ids[$index]);
+    }
+
+    return $jsonMode === TRUE
+      ? json_encode($details, JSON_PRETTY_PRINT)
+      : $details;
+  }
+
+  /**
+   * @param string $url
+   * @return array
+   */
+  private function getAnimeDetails($url)
+  {
+    $content = $this->curl($url);
+
+    $regex = array(
+      'title'       => '/hovertitle\"\>(.*?)\<\/a\>/',
+      'description' => '/10px\;\"\>(.*?)\<a/',
+      'type'        => '/Type\:\<\/span\>(.*?)\<br \/\>/',
+      'score'       => '/Score\:\<\/span\>(.*?)\<small\>/',
+      'status'      => '/Status\:\<\/span\>(.*?)\<br \/\>/',
+      'genres'      => '/Genres\:\<\/span\>(.*?)\<br \/\>/',
+      'link'        => '/\<a href\=\"(.*?)\"\>read/'
+    );
+
+    $details = array();
+
+    foreach ($regex as $key => $value)
+    {
+      $details[$key] = preg_match($value, $content, $matches)
+        ? $matches[1]
+        : NULL;
+    }
+
+    return $details;
+  }
+
+  /**
+   * @param string $url
+   * @return array
+   */
+  private function getAnimeIds($url)
+  {
+    $regex = '/rel\=\"a(.*?)\"/';
+    $content = $this->curl($url);
+
+    return preg_match_all($regex, $content, $matches)
+      ? $matches[1]
+      : NULL;
+  }
+
+  /**
+   * @param string $url
+   * @return string
+   */
+  private function curl($url)
+  {
+    $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0';
+
+    $options = array(
+      CURLOPT_RETURNTRANSFER => TRUE,
+      CURLOPT_HEADER         => FALSE,
+      CURLOPT_USERAGENT      => $userAgent,
+      CURLOPT_SSL_VERIFYPEER => FALSE,
+      CURLOPT_SSL_VERIFYHOST => FALSE
+    );
+
+    $ch = curl_init($url);
+
+    curl_setopt_array($ch, $options);
+
+    $content = curl_exec($ch);
+    $errno   = curl_errno($ch);
+    $errmsg  = curl_error($ch);
+
+    curl_close($ch);
+
+    empty($errno) OR exit($errmsg);
+
+    return $content;
+  }
+}
