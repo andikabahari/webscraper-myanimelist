f8d071a1e813aa2f94921fc63d887792f8e53599
andikabahari <38852468+andikabahari@users.noreply.github.com>
2019-05-20 11:23:48 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/src/MyAnimeList.php';
+
+// Keyword used for searching contents
+$keyword = 'code geass';
+
+// True? Result will be returned as JSON string
+// False? Result will be returned as array
+$jsonMode = FALSE;
+
+// Number of contents
+$limit = 5;
+
+$myanimelist = new MyAnimeList();
+$animes = $myanimelist->searchAnime($keyword, $jsonMode, $limit);
+
+print_r($animes);
