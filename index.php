b66ed1f4a63d15a1298e073734d654f8da5d3237
Andika Bahari <38852468+andikabahari@users.noreply.github.com>
2019-05-22 07:08:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,17 +2,38 @@
 
 require_once __DIR__ . '/src/MyAnimeList.php';
 
-// Keyword used for searching contents
-$keyword = 'code geass';
-
-// True? Result will be returned as JSON string
-// False? Result will be returned as array
-$jsonMode = FALSE;
-
-// Number of contents
-$limit = 5;
-
 $myanimelist = new MyAnimeList();
+
+// TRUE  Result will be returned as JSON string
+// FALSE Result will be returned as array
+$jsonMode = TRUE;
+
+echo "\n\n -----------------------------------------------------------\n";
+echo " Anime";
+echo "\n -----------------------------------------------------------\n\n";
+
+$id = 31964;
+$anime = $myanimelist->getAnime($id, $jsonMode);
+
+print_r($anime);
+
+$keyword = 'boku%20no%20hero%20academia';
+$limit = 5;
 $animes = $myanimelist->searchAnime($keyword, $jsonMode, $limit);
 
 print_r($animes);
+
+echo "\n\n\n -----------------------------------------------------------\n";
+echo " Manga";
+echo "\n -----------------------------------------------------------\n\n";
+
+$id = 23390;
+$manga = $myanimelist->getManga($id, $jsonMode);
+
+print_r($manga);
+
+$keyword = 'shingeki%20no%20kyojin';
+$limit = 5;
+$mangas = $myanimelist->searchManga($keyword, $jsonMode, $limit);
+
+print_r($mangas);
